b3c31f591b93e09f6537c818a497762d6880eef6
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-02-24 10:27:20 +0530

    Dynamic Method Dispatcher Example

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,45 @@
+/*
+*************************** Dynamic Method Dispatcher ******************************
+ */
+
+class Vehicle {
+    public void start(){
+        System.out.println("Engine Started");
+    }
+    public void accelerate(){
+        System.out.println("Acceleration");
+    }
+    public void brake(){
+        System.out.println("Brake");
+    }
+    public void clutch(){
+        System.out.println("Clutch");
+    }
+}
+class Car extends Vehicle{
+
+}
+
+class Bike extends Vehicle {
+    public void start(){
+        System.out.println("Bike Engine Started");
+    }
+    public void noOfSeats(int seat){
+        System.out.println(seat);
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This and Super key word in java");
+        Vehicle car = new Car();
+        car.start(); // Engine Started
+        car.accelerate(); // Acceleration
+        car.brake(); // Brake
+
+        Vehicle bike = new Bike();
+        bike.start(); // Bike Engine Started
+        //     bike.noOfSeats(2); Error
+        Bike heroHonda = new Bike();
+        heroHonda.start(); // Bike Engine Started
+        heroHonda.noOfSeats(2); // 2
     }
 }
